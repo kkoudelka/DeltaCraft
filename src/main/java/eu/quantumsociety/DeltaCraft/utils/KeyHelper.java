319af93aacea0c02c6e3f14c69536f12359c460d
Karel Koudelka <k.koudelka01@gmail.com>
2020-07-02 00:09:51 +0200

    Key helper updated

--- src/main/java/eu/quantumsociety/DeltaCraft/utils/KeyHelper.java
+++ src/main/java/eu/quantumsociety/DeltaCraft/utils/KeyHelper.java
@@ -3,15 +3,31 @@ package eu.quantumsociety.DeltaCraft.utils;
 import java.util.UUID;
 
 public class KeyHelper {
+
+    private final UUID pUid;
+    private final PluginSubmodule submodule;
+
+    public KeyHelper(UUID pUid, PluginSubmodule submodule) {
+        this.pUid = pUid;
+        this.submodule = submodule;
+    }
+
     /**
+     * Returns players key with submodule and subkey <i>players.[UUID].[subkey]</i>
      *
-     * @param playerUid Players UUID
-     * @param module Plugins submodule
-     * @param subkey Subkey
-     * @return
+     * @param subkey Subkey at the end of the key
+     * @return String
      */
-    public static String getPlayerKey(UUID playerUid, PluginSubmodule module, String subkey) {
-        String key = "players." + playerUid.toString() + "." + module.toString() + "." + subkey;
+    public String get(String subkey) {
+        return this.getPlayerKey() + "." + subkey;
+    }
+
+    /**
+     * Returns players key with submodule <i>players.[UUID].</i>
+     * @return String
+     */
+    public String getPlayerKey() {
+        String key = "players." + this.pUid.toString() + "." + this.submodule.toString();
         return key;
     }
 }
