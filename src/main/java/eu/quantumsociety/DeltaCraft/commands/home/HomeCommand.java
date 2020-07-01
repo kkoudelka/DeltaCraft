bf31590c7a3f761637eac8d349d9ad5f2ede3b59
Jiri Falta <jirkafalta@gmail.com>
2020-07-01 22:09:40 +0200

    Commands

---
+++ src/main/java/eu/quantumsociety/DeltaCraft/commands/home/HomeCommand.java
@@ -0,0 +1,11 @@
+package eu.quantumsociety.DeltaCraft.commands.home;
+
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+
+public class HomeCommand implements CommandExecutor {
+    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+        return false;
+    }
+}
---
+++ src/main/java/eu/quantumsociety/DeltaCraft/commands/spectate/SpectateCommand.java
@@ -0,0 +1,11 @@
+package eu.quantumsociety.DeltaCraft.commands.spectate;
+
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+
+public class SpectateCommand implements CommandExecutor {
+    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+        return false;
+    }
+}
