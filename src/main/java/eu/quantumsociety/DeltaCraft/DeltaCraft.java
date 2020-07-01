6215dae072a9d1b08ff0fd4688a01a5e228c0a41
Jiri Falta <jirkafalta@gmail.com>
2020-07-01 22:35:40 +0200

    Test

--- src/main/java/eu/quantumsociety/DeltaCraft/DeltaCraft.java
+++ src/main/java/eu/quantumsociety/DeltaCraft/DeltaCraft.java
@@ -1,15 +1,29 @@
 package eu.quantumsociety.DeltaCraft;
 
+import eu.quantumsociety.DeltaCraft.commands.home.HomeCommand;
+import eu.quantumsociety.DeltaCraft.commands.spectate.SpectateCommand;
 import org.bukkit.plugin.java.JavaPlugin;
+
+import java.util.logging.Logger;
 
 public class DeltaCraft extends JavaPlugin {
     @Override
     public void onEnable() {
-        getLogger().info("onEnable is called!");
+        final Logger logger = getLogger();
+
+        // Commands
+        // this.getCommand("sethome").setExecutor(new SethomeCommand());
+        this.getCommand("home").setExecutor(new HomeCommand());
+        logger.info("Home loaded");
+        this.getCommand("c").setExecutor(new SpectateCommand());
+        logger.info("Spectate loaded");
+
+        super.onEnable();
     }
 
     @Override
     public void onDisable() {
-        getLogger().info("onDisable is called!");
+        super.onDisable();
     }
+
 }
--- src/main/java/eu/quantumsociety/DeltaCraft/commands/spectate/SpectateCommand.java
+++ src/main/java/eu/quantumsociety/DeltaCraft/commands/spectate/SpectateCommand.java
@@ -3,9 +3,18 @@ package eu.quantumsociety.DeltaCraft.commands.spectate;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+
+import static org.bukkit.Bukkit.getLogger;
 
 public class SpectateCommand implements CommandExecutor {
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+        getLogger().info(String.join(" ", args));
+        if (!(sender instanceof Player)) {
+            sender.sendMessage("Only players can use this commands");
+            return false;
+        }
+
         return false;
     }
 }
