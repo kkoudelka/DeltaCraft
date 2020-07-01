fecb84813331c031a62ed3827a664d4c3119e0af
Karel Koudelka <k.koudelka01@gmail.com>
2020-07-01 21:03:01 +0200

    Initial commit

---
+++ src/main/java/eu/quantumsociety/DeltaCraft/DeltaCraft.java
@@ -0,0 +1,13 @@
+package eu.quantumsociety.DeltaCraft;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class DeltaCraft extends JavaPlugin {
+    @Override
+    public void onEnable() {
+        getLogger().info("onEnable is called!");
+    }
+    @Override
+    public void onDisable() {
+        getLogger().info("onDisable is called!");
+    }
+}
