38107b1fbc82ce13feb00f7ddeed36b0fbc37c06
Jiri Falta <jirkafalta@gmail.com>
2020-07-23 14:11:28 +0200

    Settings description 2

--- src/main/java/eu/quantumsociety/deltacraft/utils/enums/Settings.java
+++ src/main/java/eu/quantumsociety/deltacraft/utils/enums/Settings.java
@@ -1,22 +1,35 @@
 package eu.quantumsociety.deltacraft.utils.enums;
 
 public enum Settings {
-    DEBUG("system.debug"),
-    END("settings.end"),
-    SPECTATEMAXDISTANCE("settings.spectate.maxdistance"),
-    HOMEMAXHOMES("settings.home.maxhomes"),
-    KELPDEBUG("settings.kelp.debug"),
-    KELPMAXFARMS("settings.kelp.maxfarms"),
-    KELPMAXDISTANCE("settings.kelp.maxdistance");
+    DEBUG("system.debug", "Use debug mode", "BOOL"),
+    END("settings.end", "Allow access to end dimension", "BOOL"),
+    SPECTATEMAXDISTANCE("settings.spectate.maxdistance", "Maximum distance can player travel in spectator mode from starting point", "INT"),
+    HOMEMAXHOMES("settings.home.maxhomes", "Maximum homes player can create", "INT"),
+    KELPDEBUG("settings.kelp.debug", "Use debug mode in kelp farms", "BOOL"),
+    KELPMAXFARMS("settings.kelp.maxfarms", "Maximum kelp farms player can create", "INT"),
+    KELPMAXDISTANCE("settings.kelp.maxdistance", "Maximum distance of 2 points of kelp farm", "INT");
 
-    private String path;
+    private final String path;
+    private final String description;
+    private final String type;
 
-    Settings(String path) {
+    Settings(String path, String description, String type) {
         this.path = path;
+        this.description = description;
+        this.type = type;
     }
 
     public String getPath() {
         return this.path;
+    }
+
+    // Cannot use getDescription because of JetBrains already using it somewhere
+    public String getDesc() {
+        return description;
+    }
+
+    public String getType() {
+        return "[" + type + "]";
     }
 
     @Override
